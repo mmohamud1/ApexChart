8bec1331824f206cc8268d6a2ccd57c11673148b
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-25 14:18:06 +0000

    differentiate lines by color

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 var options = {
-  chart: {
-    type: 'line',
-    width: '50%',
-  },
   series: [
     {
       name: 'Session Duration',
@@ -17,28 +13,84 @@ var options = {
       data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47],
     },
   ],
-  xaxis: {
-    categories: [2000, 2001, 2002, 1994, 1995, 1996, 1997, 1998, 1999],
-  },
-  fill: {
-    type: 'gradient',
-    gradient: {
-      shadeIntensity: 1,
-      opacityFrom: 0.7,
-      opacityTo: 0.9,
-      colorStops: [
-        {
-          offset: 0,
-          color: '#EB656F',
-          opacity: 1,
-        },
-        {
-          offset: 60,
-          color: '#61DBC3',
-          opacity: 1,
-        },
-      ],
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false,
     },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    width: [5, 7, 5],
+    curve: 'straight',
+    dashArray: [0, 8, 5],
+  },
+  title: {
+    text: 'Page Statistics',
+    align: 'left',
+  },
+  legend: {
+    tooltipHoverFormatter: function (val, opts) {
+      return (
+        val +
+        ' - ' +
+        opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
+        ''
+      );
+    },
+  },
+  markers: {
+    size: 0,
+    hover: {
+      sizeOffset: 6,
+    },
+  },
+  xaxis: {
+    categories: [
+      '01 Jan',
+      '02 Jan',
+      '03 Jan',
+      '04 Jan',
+      '05 Jan',
+      '06 Jan',
+      '07 Jan',
+      '08 Jan',
+      '09 Jan',
+      '10 Jan',
+      '11 Jan',
+      '12 Jan',
+    ],
+  },
+  tooltip: {
+    y: [
+      {
+        title: {
+          formatter: function (val) {
+            return val + ' (mins)';
+          },
+        },
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val + ' per session';
+          },
+        },
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val;
+          },
+        },
+      },
+    ],
+  },
+  grid: {
+    borderColor: '#f1f1f1',
   },
 };
 
