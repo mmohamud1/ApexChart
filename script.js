76c97952b955568f03278b02646e8bd1ac6dc93a
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-25 14:14:51 +0000

    add color gradient to chart

--- script.js
+++ script.js
@@ -13,9 +13,27 @@ var options = {
     categories: [2000, 2001, 2002, 1994, 1995, 1996, 1997, 1998, 1999],
   },
   fill: {
-    colors: ['#F44336', '#E91E63', '#9C27B0'],
-  },
+    type: 'gradient',
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      colorStops: [
+        {
+          offset: 0,
+          color: '#EB656F',
+          opacity: 1,
+        },
 
+        {
+          offset: 60,
+          color: '#61DBC3',
+          opacity: 1,
+        },
+
+      ],
+    },
+  },
 };
 
 var chart = new ApexCharts(document.querySelector('#chart'), options);
